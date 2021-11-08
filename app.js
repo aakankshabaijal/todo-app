afe9c75ce536320ecc14a681600442da59e4cec5
aakankshabaijal <f20190304@goa.bits-pilani.ac.in>
2021-11-08 18:42:57 +0530

    Adding tasks working

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 console.log('Connected!');
+let toDoArray = [];
+const todoList = document.querySelector('.todoList');
+
+let userInputToDo = document.querySelector('.userInput');
+console.log(userInputToDo.value);
+
+userInputToDo.addEventListener('keyup', (event) => {
+	if (event.keyCode === 13) {
+		//when enter is pressed
+		toDoArray.push(userInputToDo.value);
+		addToDoInList(userInputToDo.value);
+		console.log(toDoArray);
+		userInputToDo.value = '';
+	}
+});
+
+const addToDoInList = (toDo) => {
+	const toDoItem = document.createElement('div');
+	toDoItem.classList.add('todoItem');
+	toDoItem.innerText = toDo;
+	todoList.appendChild(toDoItem);
+};
